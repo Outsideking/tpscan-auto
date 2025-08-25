7059c9acb820b500afa9e0b4c661902e2a07fbae
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-26 02:42:18 +0700

    Update emulator.js

--- cloud-android-system/backend/api/emulator.js
+++ cloud-android-system/backend/api/emulator.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
 
+// Mock endpoint returning Cloud Android Web URL
+router.get('/url', (req, res) => {
+    res.json({ url: "http://<ALB-DNS>:6080" });
+});
+
+module.exports = router;
