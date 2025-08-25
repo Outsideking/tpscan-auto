3928a912c8b0d5ce7d6c4042e7eeaa8ce4f87357
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-26 02:50:04 +0700

    Update sequelize.js

--- cloud-android-system/backend/sequelize.js
+++ cloud-android-system/backend/sequelize.js
@@ -1,1 +1,14 @@
+import { Sequelize } from "sequelize";
 
+const {
+  DB_HOST = "db",
+  DB_PORT = "5432",
+  DB_USER = "appuser",
+  DB_PASS = "apppass",
+  DB_NAME = "appdb",
+  NODE_ENV = "development",
+} = process.env;
+
+export const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  logging: NODE_ENV === "development" ? console.log : false,
+});
