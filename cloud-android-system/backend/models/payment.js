5dff3789d50d0270ebfaa6811be665b85aae8dfa
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-26 02:40:59 +0700

    Update payment.js

--- cloud-android-system/backend/models/payment.js
+++ cloud-android-system/backend/models/payment.js
@@ -1,1 +1,11 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../sequelize');
 
+const Payment = sequelize.define('Payment', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    userId: { type: DataTypes.INTEGER },
+    amount: { type: DataTypes.FLOAT },
+    status: { type: DataTypes.STRING }
+});
+
+module.exports = Payment;
