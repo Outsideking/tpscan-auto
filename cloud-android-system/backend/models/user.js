9bc264605902aee8fad2c92d8f6963a3fee49ae8
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-26 02:40:34 +0700

    Update user.js

--- cloud-android-system/backend/models/user.js
+++ cloud-android-system/backend/models/user.js
@@ -1,1 +1,12 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../sequelize');
 
+const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: 'user' },
+    active: { type: DataTypes.BOOLEAN, defaultValue: false }
+});
+
+module.exports = User;
