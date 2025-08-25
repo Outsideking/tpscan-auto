91f9a72403e8d600df09c598c5ad9c00867510ff
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-26 02:43:22 +0700

    Update main.js

--- cloud-android-system/frontend/js/main.js
+++ cloud-android-system/frontend/js/main.js
@@ -1,1 +1,5 @@
-
+document.getElementById("openEmulator").addEventListener("click", async () => {
+    const res = await fetch("/api/emulator/url");
+    const data = await res.json();
+    window.open(data.url, "_blank");
+});
